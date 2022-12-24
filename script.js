3bfcddc78bcbde0c601f521c1c4fa7ce692a0615
fortimindkal <bintangmuhammadhaikal@gmail.com>
2022-12-24 23:27:59 +0700

    Menambah search bar & hapus siswa

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+document.querySelector(".delete-siswa").addEventListener("click", function() {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    }
+  })
+});
+
+document.querySelector(".delete-siswa2").addEventListener("click", function() {
+  Swal.fire({
+    title: "Do you want to delete this student?",
+    type: "info",
+    showCancelButton: true,
+    confirmButtonText: "Delete It",
+    confirmButtonColor: "#ff0055",
+    cancelButtonColor: "#999999",
+    reverseButtons: true,
+    focusConfirm: false,
+    focusCancel: true
+  });
+});
+
 // Menampilkan form edit siswa saat tombol edit diklik
 document.querySelectorAll('.edit-btn').forEach((button) => {
     button.addEventListener('click', (event) => {
@@ -21,15 +55,42 @@ document.querySelectorAll('.edit-btn').forEach((button) => {
     });
   });
   
-  // Menampilkan form delete siswa saat tombol delete diklik
-  document.querySelectorAll('.delete-btn').forEach((button) => {
-    button.addEventListener('click', (event) => {
-      const studentRow = event.target.closest('tr');
-      const studentId = studentRow.dataset.studentId;
-  
-      document.querySelector('#delete-student-form').dataset.studentId = studentId;
-  
-      const modal = document.querySelector('#delete-student-modal');
-      modal.style.display = 'block';
-    });
+// Menampilkan form delete siswa saat tombol delete diklik
+document.querySelectorAll('.delete-btn').forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const studentRow = event.target.closest('tr');
+    const studentId = studentRow.dataset.studentId;
+
+    document.querySelector('#delete-student-form').dataset.studentId = studentId;
+
+    const modal = document.querySelector('#delete-student-modal');
+    modal.style.display = 'block';
   });
+});
+
+function searchStudents() {
+  // Ambil input dari form pencarian
+  let input = document.getElementById("search-input").value;
+  // Konversi input menjadi huruf kecil semua
+  input = input.toLowerCase();
+
+  // Dapatkan semua baris tabel siswa
+  let rows = document.getElementsByClassName("siswa-row");
+
+  // Lakukan looping untuk setiap baris tabel siswa
+  for (let i = 0; i < rows.length; i++) {
+    // Dapatkan nama siswa dari baris tabel yang sedang dicari
+    let siswaNama = rows[i].getElementsByTagName("td")[0];
+    // Konversi nama siswa menjadi huruf kecil semua
+    siswaNama = siswaNama.textContent.toLowerCase();
+
+    // Bandingkan nama siswa dengan input pencarian
+    if (siswaNama.indexOf(input) > -1) {
+      // Tampilkan baris tabel jika nama siswa cocok dengan input pencarian
+      rows[i].style.display = "";
+    } else {
+      // Sembunyikan baris tabel jika nama siswa tidak cocok dengan input pencarian
+      rows[i].style.display = "none";
+    }
+  }
+}
