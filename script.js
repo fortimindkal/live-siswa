8d4038f3fc6f097a33daaf3c0ae0a1e614a22ed5
fortimindkal <bintangmuhammadhaikal@gmail.com>
2022-12-25 21:19:07 +0700

    Export Table to CSV

--- script.js
+++ script.js
@@ -9,6 +9,61 @@ window.onload = function() {
   loadStudents();
 }
 
+function exportToCsv(filename, rows) {
+  var processRow = function (row) {
+    var finalVal = '';
+    for (var j = 0; j < row.length; j++) {
+      var innerValue = row[j] === null ? '' : row[j].toString();
+      if (row[j] instanceof Date) {
+        innerValue = row[j].toLocaleString();
+      };
+      var result = innerValue.replace(/"/g, '""');
+      if (result.search(/("|,|\n)/g) >= 0)
+        result = '"' + result + '"';
+      if (j > 0)
+        finalVal += ',';
+      finalVal += result;
+    }
+    return finalVal + '\n';
+  };
+
+  var csvFile = '';
+  for (var i = 0; i < rows.length; i++) {
+    csvFile += processRow(rows[i]);
+  }
+
+  var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+  if (navigator.msSaveBlob) { // IE 10+
+    navigator.msSaveBlob(blob, filename);
+  } else {
+    var link = document.createElement("a");
+    if (link.download !== undefined) { // feature detection
+      // Browsers that support HTML5 download attribute
+      var url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", filename);
+      link.style.visibility = 'hidden';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
+}
+
+function exportTableToCsv(filename) {
+  var rows = document.querySelectorAll("table tr");
+  var data = [];
+  for (var i = 0; i < rows.length; i++) {
+    var row = [];
+    var cols = rows[i].querySelectorAll("td, th");
+    for (var j = 0; j < cols.length; j++) {
+      row.push(cols[j].textContent.trim());
+    }
+    data.push(row);
+  }
+  exportToCsv(filename, data);
+}
+
 function showButtons(role) {
   // Dapatkan semua button edit dan delete
   let editButtons = document.getElementsByClassName("edit-siswa");
